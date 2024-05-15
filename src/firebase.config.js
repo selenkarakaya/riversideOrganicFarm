4f623eec56d027c348d5948546ef5dd1780680a5
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2024-05-15 10:30:52 +0100

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,23 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCF8784wT5ocdAR-xC7ub-dRTbZgtjXwfk",
-//   authDomain: "riverside-box-project.firebaseapp.com",
-//   projectId: "riverside-box-project",
-//   storageBucket: "riverside-box-project.appspot.com",
-//   messagingSenderId: "512262392511",
-//   appId: "1:512262392511:web:3b273456626fdaa06c540a",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export const db = getFirestore();
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -27,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCEY2eD1lXKLcLfKEsmKpKl_PUKDepjnrY",
-  authDomain: "riverside-3fd46.firebaseapp.com",
-  projectId: "riverside-3fd46",
-  storageBucket: "riverside-3fd46.appspot.com",
-  messagingSenderId: "240846770539",
-  appId: "1:240846770539:web:6b7e9505e3047b7ac7fc2e",
+  apiKey: "your_key",
+  authDomain: "your_domain.firebaseapp.com",
+  projectId: "your_projectID",
+  storageBucket: "your_project.appspot.com",
+  messagingSenderId: "your_ID",
+  appId: "your_appID",
 };
 
 // Initialize Firebase
